24f336e4c32b78099c93dde28e8416c553b7666a
vikkixzl <trutenkoviktoria256@gmail.com>
2026-05-19 14:22:11 +0300

    додано конструктори для Product

--- Project/Product.cs
+++ Project/Product.cs
@@ -12,9 +12,73 @@ namespace Project
         private double price;
         private int quantity;
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        static Product()
+        {
+            Console.WriteLine("Статичний конструктор Product");
+        }
+
+        private Product(bool secret)
+        {
+            Console.WriteLine("Закритий конструктор Product");
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+
+            Console.WriteLine("Конструктор з параметрами Product");
+        }
+
+        public Product(Product other)
+        {
+            name = other.name;
+            price = other.price;
+            quantity = other.quantity;
+
+            Console.WriteLine("Конструктор копіювання Product");
+        }
+
+        public Product(string name) : this()
+        {
+            this.name = name;
+
+            Console.WriteLine("Конструктор з викликом іншого конструктора Product");
+        }
+
         public void ShowProductInfo()
         {
-            Console.WriteLine("Інформація про товар");
+            Console.WriteLine($"Товар: {name}");
+            Console.WriteLine($"Ціна: {price}");
+            Console.WriteLine($"Кількість: {quantity}");
+        }
+
+        public Product()
+        {
+            name = "Невідомий товар";
+            price = 0;
+            quantity = 0;
+
+            Console.WriteLine("Конструктор без параметрів Product");
         }
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -10,15 +10,14 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             Console.WriteLine("ПІБ студента: Трутенко Вікторія Миколаївна");
             Console.WriteLine("Курс: 1");
             Console.WriteLine("Група: ІПЗ-11");
             Console.WriteLine("Варіант: 45");
             Console.WriteLine("Версія 1");
-
             Console.WriteLine("Старт імітації");
-
             Console.WriteLine("Фініш імітації");
         }
     }
