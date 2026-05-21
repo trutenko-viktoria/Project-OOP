c8d75e5e90b67f8c5ebd7bd315503a35a2993f8a
vikkixzl <trutenkoviktoria256@gmail.com>
2026-05-21 18:07:28 +0300

    додано введення даних клієнта та вибір кількох товарів

--- Project/Program.cs
+++ Project/Program.cs
@@ -19,50 +19,35 @@ namespace Project
             Console.WriteLine("===== Інтернет-магазин одягу =====");
             Console.WriteLine();
 
-            // Клієнт
+            Console.WriteLine("Реєстрація клієнта");
+
+            Console.Write("Введіть ім'я та прізвище: ");
+            string clientName = Console.ReadLine();
+
+            Console.Write("Введіть номер телефону: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Введіть адресу доставки: ");
+            string deliveryAddress = Console.ReadLine();
+
             Cart clientCart = new Cart();
 
             Client client = new Client(
-                "Трутенко Вікторія",
-                "+380991112233",
+                clientName,
+                phoneNumber,
                 clientCart);
 
             client.Register();
 
             Console.WriteLine();
 
-            // Каталог товарів
             List<Product> storeProducts = new List<Product>();
 
-            storeProducts.Add(
-                new Product(
-                    "Худі",
-                    1200,
-                    5,
-                    "Тепле oversize худі",
-                    "M",
-                    "Сірий"));
-
-            storeProducts.Add(
-                new Product(
-                    "Сукня",
-                    1800,
-                    3,
-                    "Чорна вечірня сукня",
-                    "S",
-                    "Чорний"));
-
-            storeProducts.Add(
-                new Product(
-                    "Джинси",
-                    1500,
-                    2,
-                    "Широкі сині джинси",
-                    "M",
-                    "Синій"));
+            storeProducts.Add(new Product("Худі", 1200, 5, "Тепле oversize худі", "M", "Сірий"));
+            storeProducts.Add(new Product("Сукня", 1800, 3, "Чорна вечірня сукня", "S", "Чорний"));
+            storeProducts.Add(new Product("Джинси", 1500, 2, "Широкі сині джинси", "M", "Синій"));
 
             List<Order> storeOrders = new List<Order>();
-
             Manager storeManager = new Manager("Максим", 5);
 
             OnlineStore store = new OnlineStore(
@@ -72,67 +57,80 @@ namespace Project
 
             store.OpenStore();
 
+            string choice;
+
+            do
+            {
+                Console.WriteLine();
+                store.ShowCatalog();
+
+                Console.WriteLine("Введіть номер товару для додавання в корзину");
+                Console.WriteLine("або номер з + для повної інформації, наприклад 1+");
+                Console.WriteLine("0 - завершити вибір товарів");
+
+                choice = Console.ReadLine();
+
+                if (choice == "0")
+                {
+                    break;
+                }
+
+                if (choice.Contains("+"))
+                {
+                    string numberPart = choice.Replace("+", "");
+                    int productIndex = Convert.ToInt32(numberPart) - 1;
+
+                    store.ShowProductDetails(productIndex);
+                }
+                else
+                {
+                    int productIndex = Convert.ToInt32(choice) - 1;
+
+                    Product selectedProduct = store.GetProductByIndex(productIndex);
+
+                    if (selectedProduct != null && selectedProduct.IsAvailable())
+                    {
+                        Product productForCart = new Product(
+                            selectedProduct.Name,
+                            selectedProduct.Price,
+                            1,
+                            selectedProduct.Description,
+                            selectedProduct.Size,
+                            selectedProduct.Color);
+
+                        client.AddToCart(productForCart);
+                        selectedProduct.DecreaseQuantity();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Товар відсутній або вибрано неправильний номер");
+                    }
+                }
+
+            } while (choice != "0");
+
             Console.WriteLine();
 
-            // Каталог
-            store.ShowCatalog();
+            client.Cart.CalculateTotalPrice();
+            client.Cart.ShowCartInfo();
 
-            Console.WriteLine("Введіть номер товару або номер з + для повної інформації:");
+            Console.WriteLine();
 
-            string userChoice = Console.ReadLine();
+            client.MakeOrder();
 
-            if (userChoice.Contains("+"))
-            {
-                string numberPart = userChoice.Replace("+", "");
+            Payment payment = new Payment(client.Cart.TotalPrice, false);
+            payment.Pay();
 
-                int productIndex = Convert.ToInt32(numberPart) - 1;
+            Delivery delivery = new Delivery(deliveryAddress, "оформлена");
+            delivery.Deliver();
 
-                store.ShowProductDetails(productIndex);
-            }
-            else
-            {
-                int productIndex = Convert.ToInt32(userChoice) - 1;
+            Order order = new Order(101, payment, delivery);
+            store.RegisterOrder(order);
 
-                Product selectedProduct = store.GetProductByIndex(productIndex);
+            Console.WriteLine();
 
-                if (selectedProduct != null)
-                {
-                    client.AddToCart(selectedProduct);
-
-                    client.Cart.CalculateTotalPrice();
-
-                    Console.WriteLine();
-
-                    client.Cart.ShowCartInfo();
-
-                    Console.WriteLine();
-
-                    client.MakeOrder();
-
-                    Payment payment = new Payment(
-                        client.Cart.TotalPrice,
-                        false);
-
-                    payment.Pay();
-
-                    Delivery delivery = new Delivery(
-                        "м. Київ, Нова пошта №5",
-                        "оформлена");
-
-                    delivery.Deliver();
-
-                    Order order = new Order(
-                        101,
-                        payment,
-                        delivery);
-
-                    store.RegisterOrder(order);
-
-                    Console.WriteLine();
-
-                    Console.WriteLine("Замовлення успішно оформлено");
-                }
-            }
+            Console.WriteLine("Замовлення успішно оформлено");
+            Console.WriteLine("Фініш імітації");
 
             Console.WriteLine();
         }
