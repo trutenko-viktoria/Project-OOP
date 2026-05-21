618eda961634e76b47acae26ea3ad25958b6cd5d
vikkixzl <trutenkoviktoria256@gmail.com>
2026-05-21 18:10:24 +0300

    додано абстрактний клас User

---
+++ Project/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Project
+{
+    internal abstract class User
+    {
+        protected string fullName;
+        protected string phoneNumber;
+
+        public string FullName
+        {
+            get { return fullName; }
+            set { fullName = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public User()
+        {
+            fullName = "Невідомий користувач";
+            phoneNumber = "Не вказано";
+        }
+
+        public User(string fullName, string phoneNumber)
+        {
+            this.fullName = fullName;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public abstract void ShowUserRole();
+    }
+}
